7d53645aee030bd639766c6862504c50cb493c2e
dianaparr <dianaparra.9611@gmail.com>
2021-07-05 16:11:02 -0500

    Task advanced, output ok, sub-matches used ()

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 # escape square brackets with backslash
 # .: any one character except newline
 # *?: 0 or more times
-puts ARGV[0].scan(/\[from:.*?\] \[to:.*?\] \[flags:.*?\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
