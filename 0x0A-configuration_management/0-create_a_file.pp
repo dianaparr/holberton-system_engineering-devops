fd2210365249e31b0b9b1cdda7bc857efb4db3cf
dianaparr <dianaparra.9611@gmail.com>
2021-08-13 11:45:40 -0500

    Uploading a script to create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create a file in the path specified
+file { 'File' :
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
