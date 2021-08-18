1a69f878b76a240884511d6f1500452d401d2fb0
dianaparr <dianaparra.9611@gmail.com>
2021-08-18 14:24:28 -0500

    Adding comments

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,16 +5,19 @@ package { 'get nginx' :
   provider => 'apt',
 }
 
+# Verificated if nginx have been install
 service { 'nginx':
   ensure  => running,
   enabled => true,
 }
 
+# Command to redirect page
 exec { 'command one: to redirect' :
   provider => shell,
   command  => 'sudo sed -i "/server_name _;/ a \\\trewrite ^/redirect_me https://www.nginx.com/resources/library/complete-nginx-cookbook/ permanent;" /etc/nginx/sites-available/default',
 }
 
+# Command to show Holberton School in index
 exec { 'command two: index' :
   provider => shell,
   command  => 'echo "Holberton School" > /var/www/html/index.nginx-debian.html',
