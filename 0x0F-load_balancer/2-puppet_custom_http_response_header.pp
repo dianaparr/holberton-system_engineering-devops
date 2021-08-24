9b2364781e7505f5680703a0878f494643991fe9
dianaparr <dianaparra.9611@gmail.com>
2021-08-24 12:01:21 -0500

    Delete line break and tab

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,37 +1,37 @@
 # Script that configure an Nginx server
-exec { 'update' :
+exec { 'update':
   command  => 'sudo apt-get -y update',
   provider => shell,
 }
 
-package { 'nginx' :
+package { 'nginx':
   ensure  => 'installed',
   require => Exec['update'],
 }
 
-file_line { 'redirect' :
+file_line { 'redirect':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'server_name _;',
-  line    => '\trewrite ^/redirect_me https://www.nginx.com/resources/library/complete-nginx-cookbook/ permanent;',
+  line    => 'rewrite ^/redirect_me https://www.nginx.com/resources/library/complete-nginx-cookbook/ permanent;',
   require => Package['nginx'],
 }
 
-file_line { 'header' :
+file_line { 'header':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
-  after   => '\trewrite ^/redirect_me https://www.nginx.com/resources/library/complete-nginx-cookbook/ permanent;',
-  line    => '\tadd_header X-Served-By $hostname;',
+  after   => 'rewrite ^/redirect_me https://www.nginx.com/resources/library/complete-nginx-cookbook/ permanent;',
+  line    => 'add_header X-Served-By $hostname;',
   require => Package['nginx'],
 }
 
-file { '/var/www/html/index.nginx-debian.html' :
+file { '/var/www/html/index.nginx-debian.html':
   ensure  => 'file',
   content => 'Holberton School',
   require => Package['nginx'],
 }
 
-service { 'nginx' :
+service { 'nginx':
   ensure  => 'running',
   enable  => true,
   require => Package['nginx'],
