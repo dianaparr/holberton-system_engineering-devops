ad12acd909b87cb5dfec15326327d20835667bf3
dianaparr <dianaparra.9611@gmail.com>
2021-08-23 22:07:34 -0500

    Retry uploading task advanced

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,20 @@
 # Script that configure an Nginx server
+exec { 'update' :
+  command  => 'sudo apt-get -y update',
+  provider => shell
+}
+
+package { 'nginx' :
+  ensure => 'installed',
+}
+
 exec { 'header' :
   provider => shell,
-  command  => "sudo apt-get -y update;
-               sudo apt-get -y install nginx;
-               sudo sed -i '/rewrite/ a \\\n\tadd_header X-Served-By ${hostname};' /etc/nginx/sites-available/default;
-               sudo /etc/init.d/nginx restart",
+  command  => "sudo sed -i '/rewrite/ a \\\n\tadd_header X-Served-By $hostname;' /etc/nginx/sites-available/default;"
+}
+
+service { 'nginx' :
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
 }
